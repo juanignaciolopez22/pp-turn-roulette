c846c84ffd912d864fea0c2003e7a833e3520406
Juani <70241423+juanignaciolopez22@users.noreply.github.com>
2026-07-23 11:39:45 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -312,11 +312,11 @@ const Index = () => {
                   <div className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50">
                     <div className="bg-gray-900 p-10 rounded-lg text-center shadow-2xl max-w-2xl w-full border border-gray-700">
                       <img
-                        src="/pp-turn-roulette/draco-sql.jfif"
+                        src="/pp-turn-roulette/el-conejo.jfif"
                         alt="draco"
                         className="w-80 mx-auto mb-8"
                       />
-                      <h2 className="text-3xl font-bold mb-6 text-white">SELECT casino.money FROM casino WHERE color=negro</h2>
+                      <h2 className="text-3xl font-bold mb-6 text-white">El conejo malo lo hizo de nuevo!</h2>
                       <button
                         onClick={() => setShowLauModal(false)}
                         className="px-6 py-3 bg-green-500 text-white rounded hover:bg-green-600 transition text-lg"
